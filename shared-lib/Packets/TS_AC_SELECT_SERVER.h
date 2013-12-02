ac9f3a3aadc88ff84ec3b8501f02d606e1f3dfd7
glandu2 <glandu2epvp@gmail.com>
2013-12-02 23:53:34 +0100

    Split TS_AC_SELECT_SERVER for DES and AES versions

--- shared-lib/Packets/TS_AC_SELECT_SERVER.h
+++ shared-lib/Packets/TS_AC_SELECT_SERVER.h
@@ -7,18 +7,18 @@
 struct TS_AC_SELECT_SERVER : public TS_MESSAGE
 {
 	uint16_t result;
-	union {
-		struct {
-			int64_t one_time_key;
-			uint32_t pending_time;
-		} v1;
-		struct {
-				int32_t encrypted_data_size;
-				unsigned char encrypted_data[16];
-				uint32_t unknown;
-				uint32_t pending_time;
-		} v2;
-	};
+	int64_t one_time_key;
+	uint32_t pending_time;
+	static const uint16_t packetID = 10024;
+};
+
+struct TS_AC_SELECT_SERVER_V2 : public TS_MESSAGE
+{
+	uint16_t result;
+	int32_t encrypted_data_size;
+	unsigned char encrypted_data[16];
+	uint32_t pending_time;
+	uint32_t unknown;
 	static const uint16_t packetID = 10024;
 };
 #pragma pack(pop)
