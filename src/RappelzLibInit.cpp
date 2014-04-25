80d985954c2408813f07af8c27c35663fa624ae0
glandu2 <glandu2epvp@gmail.com>
2014-04-25 23:43:37 +0200

    Disable SIGPIPE on unix

--- src/RappelzLibInit.cpp
+++ src/RappelzLibInit.cpp
@@ -2,7 +2,19 @@
 #include "RappelzLibConfig.h"
 #include "Log.h"
 
+#ifdef __unix
+#include <signal.h>
+#endif
+
+void disableSigPipe() {
+#ifdef __unix
+	signal(SIGPIPE, SIG_IGN);
+#endif
+}
+
 bool RappelzLibInit(int argc, char **argv, ClientConfigInit configDeclareInitCallback) {
+	disableSigPipe();
+
 	RappelzLibConfig::init();
 	if(configDeclareInitCallback)
 		configDeclareInitCallback();
