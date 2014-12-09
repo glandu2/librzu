2bd7fae25ab4ca8c2c155b83c06b2764a5586f60
glandu2 <glandu2epvp@gmail.com>
2014-12-09 01:30:37 +0100

    Remove warn log (used for debug)

--- src/Pipe.cpp
+++ src/Pipe.cpp
@@ -30,7 +30,6 @@ int Pipe::connect_impl(uv_connect_t* connectRequest, const std::string & hostNam
 int Pipe::bind_impl(const std::string& interfaceIp, uint16_t port) {
 	uv_pipe_init(getLoop(), &pipe, false);
 
-	warn("Bind on %s\n", interfaceIp.c_str());
 	return uv_pipe_bind(&pipe, interfaceIp.c_str());
 }
 
