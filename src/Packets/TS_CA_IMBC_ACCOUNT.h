086a40a7bac7e32835280f43030c3d3b95ad971f
glandu2 <glandu2epvp@gmail.com>
2015-04-28 01:05:02 +0200

    IMBC account login: fix type and add RSA packet

--- src/Packets/TS_CA_IMBC_ACCOUNT.h
+++ src/Packets/TS_CA_IMBC_ACCOUNT.h
@@ -7,8 +7,22 @@
 struct TS_CA_IMBC_ACCOUNT : public TS_MESSAGE
 {
 	char account[61];
-	char password[48];
+	unsigned char password[48];
 	static const uint16_t packetID = 10012;
+};
+
+struct TS_CA_IMBC_ACCOUNT_RSA : public TS_MESSAGE
+{
+		char account[61];
+		unsigned int password_size;
+		unsigned char password[64];
+		static const int packetID = 10012;
+
+		struct AdditionalInfo
+		{
+				char type;
+				unsigned short size;
+		};
 };
 #pragma pack(pop)
 
