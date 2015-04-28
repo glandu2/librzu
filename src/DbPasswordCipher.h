46d700f6f24a80049178e9ef9b519f2f09d006fe
glandu2 <glandu2epvp@gmail.com>
2015-04-28 23:35:04 +0200

    Finish database password handling

---
+++ src/DbPasswordCipher.h
@@ -0,0 +1,14 @@
+#ifndef DBPASSWORDCIPHER_H
+#define DBPASSWORDCIPHER_H
+
+#include "Extern.h"
+#include <vector>
+#include <string>
+
+class RZU_EXTERN DbPasswordCipher
+{
+public:
+	static std::string decrypt(std::vector<unsigned char> cryptedPassword);
+};
+
+#endif // DBPASSWORDCIPHER_H
