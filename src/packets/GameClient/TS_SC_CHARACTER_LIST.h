4b0fdf15fc7e2ab4b787620cc1ce245a4e57e3a9
glandu2 <glandu2epvp@gmail.com>
2015-10-04 02:29:59 +0200

    Use one array for wear_info data of character info

--- src/packets/GameClient/TS_SC_CHARACTER_LIST.h
+++ src/packets/GameClient/TS_SC_CHARACTER_LIST.h
@@ -8,12 +8,13 @@
 	simple_ (uint32_t, sex) \
 	simple_ (uint32_t, race) \
 	array_  (uint32_t, model_id, 5) \
-	simple_ (uint32_t, hair_color_index, version >= EPIC_4_1, 0) \
-	simple_ (uint32_t, hair_color_rgb, version >= EPIC_4_1, 0) \
-	simple_ (uint32_t, hide_equip_flag, version >= EPIC_4_1, 0) \
-	simple_ (uint32_t, texture_id, version >= EPIC_4_1, 0) \
-	array_  (uint32_t, wear_info, 14) \
-	array_  (uint32_t, wear_info_2, 10, version >= EPIC_4_1, {0}) \
+	simple_ (uint32_t, hair_color_index, version >= EPIC_4_1) \
+	simple_ (uint32_t, hair_color_rgb, version >= EPIC_4_1) \
+	simple_ (uint32_t, hide_equip_flag, version >= EPIC_4_1) \
+	simple_ (uint32_t, texture_id, version >= EPIC_4_1) \
+	array_(def)(uint32_t, wear_info, 24) \
+	array_(impl)(uint32_t, wear_info, 14, version < EPIC_4_1) \
+	array_(impl)(uint32_t, wear_info, 24, version >= EPIC_4_1) \
 	simple_ (uint32_t, level) \
 	simple_ (uint32_t, job) \
 	simple_ (uint32_t, job_level) \
@@ -23,15 +24,17 @@
 	simple_ (uint32_t, permission) \
 	simple_ (uint8_t, is_banned) \
 	string_ (name, 19) \
-	simple_ (uint32_t, skin_color, version >= EPIC_4_1, 0) \
+	simple_ (uint32_t, skin_color, version >= EPIC_4_1) \
 	array_  (char, szCreateTime, 30) \
 	array_  (char, szDeleteTime, 30) \
-	array_  (uint32_t, wear_item_enhance_info, 14) \
-	array_  (uint32_t, wear_item_enhance_info_2, 10, version >= EPIC_4_1, {0}) \
-	array_  (uint32_t, wear_item_level_info, 14) \
-	array_  (uint32_t, wear_item_level_info_2, 10, version >= EPIC_4_1, {0}) \
-	array_  (uint8_t, wear_item_elemental_type, 24, version >= EPIC_4_1, {0}) \
-	array_  (uint32_t, wear_appearance_code, 24, version >= EPIC_8_1, {0})
+	array_(def)(uint32_t, wear_item_enhance_info, 24) \
+	array_(impl)(uint32_t, wear_item_enhance_info, 14, version < EPIC_4_1) \
+	array_(impl)(uint32_t, wear_item_enhance_info, 24, version >= EPIC_4_1) \
+	array_(def)(uint32_t, wear_item_level_info, 24) \
+	array_(impl)(uint32_t, wear_item_level_info, 14, version < EPIC_4_1) \
+	array_(impl)(uint32_t, wear_item_level_info, 24, version >= EPIC_4_1) \
+	array_  (uint8_t, wear_item_elemental_type, 24, version >= EPIC_4_1) \
+	array_  (uint32_t, wear_appearance_code, 24, version >= EPIC_8_1)
 
 CREATE_STRUCT(LOBBY_CHARACTER_INFO); // struct is 304 bytes long in epic2
 
