0902a3e7104ef26698ee2fa9f4106d0366aa112f
glandu2 <glandu2epvp@gmail.com>
2015-10-28 20:08:50 +0100

    packet: rename wear info field to match TS_SC_CHARACTER_LIST

--- src/packets/GameClient/TS_SC_WEAR_INFO.h
+++ src/packets/GameClient/TS_SC_WEAR_INFO.h
@@ -5,17 +5,17 @@
 
 #define TS_SC_WEAR_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
 	simple_ (uint32_t, handle) \
-	array_(def)(uint32_t, itemCode, 24) \
-	array_(impl)(uint32_t, itemCode, 14, version < EPIC_4_1) \
-	array_(impl)(uint32_t, itemCode, 24, version >= EPIC_4_1) \
-	array_(def)(uint32_t, itemEnhance, 24) \
-	array_(impl)(uint32_t, itemEnhance, 14, version < EPIC_4_1) \
-	array_(impl)(uint32_t, itemEnhance, 24, version >= EPIC_4_1) \
-	array_(def)(uint32_t, itemLevel, 24) \
-	array_(impl)(uint32_t, itemLevel, 14, version < EPIC_4_1) \
-	array_(impl)(uint32_t, itemLevel, 24, version >= EPIC_4_1) \
-	array_(uint8_t, elemental_effect_type, 24, version >= EPIC_4_1) \
-	array_(uint32_t, appearance_code, 24, version >= EPIC_8_1)
+	array_(def)(uint32_t, wear_info, 24) \
+	array_(impl)(uint32_t, wear_info, 14, version < EPIC_4_1) \
+	array_(impl)(uint32_t, wear_info, 24, version >= EPIC_4_1) \
+	array_(def)(uint32_t, wear_item_enhance_info, 24) \
+	array_(impl)(uint32_t, wear_item_enhance_info, 14, version < EPIC_4_1) \
+	array_(impl)(uint32_t, wear_item_enhance_info, 24, version >= EPIC_4_1) \
+	array_(def)(uint32_t, wear_item_level_info, 24) \
+	array_(impl)(uint32_t, wear_item_level_info, 14, version < EPIC_4_1) \
+	array_(impl)(uint32_t, wear_item_level_info, 24, version >= EPIC_4_1) \
+	array_(uint8_t, wear_item_elemental_type, 24, version >= EPIC_4_1) \
+	array_(uint32_t, wear_appearance_code, 24, version >= EPIC_8_1)
 
 CREATE_PACKET(TS_SC_WEAR_INFO, 202);
 
