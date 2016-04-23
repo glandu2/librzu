4442c90cd510c77d11be3af0532a71b0789b0f2b
glandu2 <glandu2epvp@gmail.com>
2016-04-23 12:37:26 +0200

    Rewrite TS_SC_ENTER to serializable format
    Add also EncodedInt and 2 encodings to handle values encoded before being
    send to the client

---
+++ src/lib/Packet/EncodedInt.h
@@ -0,0 +1,38 @@
+#ifndef PACKETS_ENCODEDINT_H
+#define PACKETS_ENCODEDINT_H
+
+#include <stdint.h>
+
+template<class Encoder>
+class EncodedInt {
+public:
+	EncodedInt() {}
+	EncodedInt(uint32_t v) : value(v) {}
+	EncodedInt(const EncodedInt& v) : value(v.value) {}
+	
+	EncodedInt operator=(uint32_t v) { value = v; return *this; }
+	EncodedInt operator=(EncodedInt v) { value = v.value; return *this; }
+	bool operator==(uint32_t v) const { return v == value; }
+	bool operator==(EncodedInt v) const { return v.value == value; }
+	
+	operator uint32_t() const { return value; }
+
+	uint32_t getSize(int version) const {
+		return Encoder::getSize(version);
+	}
+
+	template<class T>
+	void serialize(T* buffer) const {
+		Encoder::serialize<T>(buffer, value);
+	}
+
+	template<class T>
+	void deserialize(T* buffer) {
+		Encoder::deserialize<T>(buffer, value);
+	}
+	
+private:
+	uint32_t value;
+};
+
+#endif // PACKETS_ENCODEDINT_H
--- src/packets/GameClient/TS_SC_ENTER.h
+++ src/packets/GameClient/TS_SC_ENTER.h
@@ -1,117 +1,130 @@
 #ifndef PACKETS_TS_SC_ENTER_H
 #define PACKETS_TS_SC_ENTER_H
 
-#include "Packet/PacketBaseMessage.h"
-#include "PacketEnums.h"
+#include "Packet/PacketDeclaration.h"
+#include "Packet/EncodedInt.h"
+#include "Packet/EncodingRandomized.h"
+#include "Packet/EncodingScrambled.h"
 
-#pragma pack(push, 1)
-struct TS_SC_ENTER : public TS_MESSAGE
-{
-	char type;
-	unsigned int handle;
-	float x;
-	float y;
-	float z;
-	char layer;
-	char ObjType;
-
-	struct TS_ITEM_PICK_UP_ORDER
-	{
-		unsigned int drop_time;
-		unsigned int hPlayer[3];
-		int nPartyID[3];
-	};
-/*
-	struct ItemInfo             //ObjType = 2, type = 2
-	{
-		int code;
-		long long int cnt;
-		TS_ENTER::TS_ITEM_PICK_UP_ORDER pick_up_order;
-	};
-
-	struct SkillInfo            //ObjType  = 5, type = 2
-	{
-		unsigned int caster;
-		unsigned int start_time;
-		int skill_num;
-	};
-
-	struct FieldPropInfo        //ObjType = 6, type = 2
-	{
-		int prop_id;
-		float fZOffset;
-		float fRotateX;
-		float fRotateY;
-		float fRotateZ;
-		float fScaleX;
-		float fScaleY;
-		float fScaleZ;
-		bool bLockHeight;
-		float fLockHeight;
-	};
-*/
-	struct CreatureInfo         //interm size = 38
-	{
-		unsigned int status;
-		float face_direction;
-		int hp;
-		int max_hp;
-		int mp;
-		int max_mp;
-		int level;
-		char race;
-		unsigned int skin_color;
-		bool is_first_enter;
-		int energy;
-	};
-/*
-	struct MonsterInfo         //ObjType = 3, type = 1
-	{
-		TS_ENTER::CreatureInfo baseclass_0;
-		RandomizedInt monster_id;
-		bool is_tamed;
-	};
-
-	struct SummonInfo          //ObjType = 4, type = 1
-	{
-		TS_ENTER::CreatureInfo baseclass_0;
-		unsigned int master_handle;
-		RandomizedInt summon_code;
-		char szName[19];
-		char enhance;
-	};
-
-	struct NPCInfo            //ObjType = 1, type = 1
-	{
-		TS_ENTER::CreatureInfo baseclass_0;
-		RandomizedInt npc_id;
-	};
-*/
-	struct PlayerInfo         //ObjType = 0, type = 0
-	{
-		CreatureInfo baseclass_0;
-		char sex;
-		int faceId;
-		int faceTextureId;
-		int hairId;
-		int hairColorIndex;
-		unsigned int hairColorRGB;
-		unsigned int hideEquipFlag;
-		char szName[19];
-		unsigned short job_id;
-		unsigned int ride_handle;
-		int guild_id;
-	};
-/*
-	struct PetInfo             //ObjType = 7, type = 1
-	{
-		TS_ENTER::CreatureInfo baseclass_0;
-		unsigned int master_handle;
-		RandomizedInt pet_code;
-		char szName[19];
-	};*/
-	static const int packetID = 3;
+enum TS_SC_ENTER__OBJ_TYPE : uint8_t {
+	EOT_Player,
+	EOT_NPC,
+	EOT_Item,
+	EOT_Monster,
+	EOT_Summon,
+	EOT_Skill,
+	EOT_FieldProp,
+	EOT_Pet
 };
-#pragma pack(pop)
+
+#define TS_SC_ENTER__ITEM_PICK_UP_ORDER_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (uint32_t, drop_time) \
+	array_  (uint32_t, hPlayer, 3) \
+	array_  (int32_t, nPartyID, 3)
+CREATE_STRUCT(TS_SC_ENTER__ITEM_PICK_UP_ORDER);
+
+#define TS_SC_ENTER__ITEM_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (EncodedInt<EncodingRandomized>, code) \
+	simple_ (uint64_t, count) \
+	simple_ (TS_SC_ENTER__ITEM_PICK_UP_ORDER, pick_up_order)
+CREATE_STRUCT(TS_SC_ENTER__ITEM_INFO);
+
+#define TS_SC_ENTER__SKILL_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (uint32_t, casterHandle) \
+	simple_ (uint32_t, startTime) \
+	simple_ (uint32_t, skillId)
+CREATE_STRUCT(TS_SC_ENTER__SKILL_INFO);
+
+#define TS_SC_ENTER__FIELD_PROP_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (uint32_t, prop_id) \
+	simple_ (float, fZOffset) \
+	simple_ (float, fRotateX) \
+	simple_ (float, fRotateY) \
+	simple_ (float, fRotateZ) \
+	simple_ (float, fScaleX) \
+	simple_ (float, fScaleY) \
+	simple_ (float, fScaleZ) \
+	simple_ (bool, bLockHeight) \
+	simple_ (float, fLockHeight)
+CREATE_STRUCT(TS_SC_ENTER__FIELD_PROP_INFO);
+
+#define TS_SC_ENTER__CREATURE_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (uint32_t, status) \
+	simple_ (float, face_direction) \
+	simple_ (int32_t, hp) \
+	simple_ (int32_t, max_hp) \
+	simple_ (int32_t, mp) \
+	simple_ (int32_t, max_mp) \
+	simple_ (int32_t, level) \
+	simple_ (uint8_t, race) \
+	simple_ (uint32_t, skin_color) \
+	simple_ (bool, is_first_enter) \
+	simple_ (int32_t, energy)
+CREATE_STRUCT(TS_SC_ENTER__CREATURE_INFO);
+
+#define TS_SC_ENTER__MONSTER_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (TS_SC_ENTER__CREATURE_INFO, creatureInfo) \
+	simple_ (EncodedInt<EncodingScrambled>, monster_id) \
+	simple_ (bool, is_tamed)
+CREATE_STRUCT(TS_SC_ENTER__MONSTER_INFO);
+
+#define TS_SC_ENTER__SUMMON_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (TS_SC_ENTER__CREATURE_INFO, creatureInfo) \
+	simple_ (uint32_t, master_handle) \
+	simple_ (EncodedInt<EncodingRandomized>, summon_code) \
+	string_ (szName, 19) \
+	simple_ (uint8_t, enhance)
+CREATE_STRUCT(TS_SC_ENTER__SUMMON_INFO);
+
+#define TS_SC_ENTER__NPC_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (TS_SC_ENTER__CREATURE_INFO, creatureInfo) \
+	simple_ (EncodedInt<EncodingRandomized>, npc_id)
+CREATE_STRUCT(TS_SC_ENTER__NPC_INFO);
+
+#define TS_SC_ENTER__PLAYER_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (TS_SC_ENTER__CREATURE_INFO, creatureInfo) \
+	simple_ (uint8_t, sex) \
+	simple_ (uint32_t, faceId) \
+	simple_ (uint32_t, faceTextureId) \
+	simple_ (uint32_t, hairId) \
+	simple_ (uint32_t, hairColorIndex) \
+	simple_ (uint32_t, hairColorRGB) \
+	simple_ (uint32_t, hideEquipFlag) \
+	string_ (szName, 19) \
+	simple_ (uint16_t, job_id) \
+	simple_ (uint32_t, ride_handle) \
+	simple_ (uint32_t, guild_id) \
+	simple_ (uint64_t, unknown, version >= EPIC_9_2)
+CREATE_STRUCT(TS_SC_ENTER__PLAYER_INFO);
+
+#define TS_SC_ENTER__PET_INFO_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (TS_SC_ENTER__CREATURE_INFO, creatureInfo) \
+	simple_ (uint32_t, master_handle) \
+	simple_ (EncodedInt<EncodingRandomized>, pet_code) \
+	string_ (szName, 19)
+CREATE_STRUCT(TS_SC_ENTER__PET_INFO);
+
+#define TS_SC_ENTER_DEF(simple_, array_, dynarray_, count_, string_, dynstring_) \
+	simple_ (uint8_t, type) /* 0 = static object, 1 = movable object, 2 = client object (ArObject::ObjectType) */ \
+	simple_ (uint32_t, handle) \
+	simple_ (float, x) \
+	simple_ (float, y) \
+	simple_ (float, z) \
+	simple_ (uint8_t, layer) \
+	simple_ (TS_SC_ENTER__OBJ_TYPE, objType) \
+	simple_ (TS_SC_ENTER__PLAYER_INFO    , playerInfo   , objType == EOT_Player) \
+	simple_ (TS_SC_ENTER__NPC_INFO       , npcInfo      , objType == EOT_NPC) \
+	simple_ (TS_SC_ENTER__ITEM_INFO      , itemInfo     , objType == EOT_Item) \
+	simple_ (TS_SC_ENTER__MONSTER_INFO   , monsterInfo  , objType == EOT_Monster) \
+	simple_ (TS_SC_ENTER__SUMMON_INFO    , summonInfo   , objType == EOT_Summon) \
+	simple_ (TS_SC_ENTER__SKILL_INFO     , skillInfo    , objType == EOT_Skill) \
+	simple_ (TS_SC_ENTER__FIELD_PROP_INFO, fieldPropInfo, objType == EOT_FieldProp) \
+	simple_ (TS_SC_ENTER__PET_INFO       , petInfo      , objType == EOT_Pet)
+
+#define TS_SC_ENTER_ID(X) \
+	X(3, version < EPIC_9_2) \
+	X(63, version >= EPIC_9_2)
+
+CREATE_PACKET_VER_ID(TS_SC_ENTER);
 
 #endif // PACKETS_TS_SC_ENTER_H
