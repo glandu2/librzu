50c358a6ba2f5442f7daa3daec856113d1740508
glandu2 <glandu2epvp@gmail.com>
2016-08-23 23:55:21 +0200

    Fix TS_SC_REGION_ACK packet

--- src/packets/GameClient/TS_SC_REGION_ACK.h
+++ src/packets/GameClient/TS_SC_REGION_ACK.h
@@ -1,11 +1,11 @@
 #ifndef PACKETS_TS_SC_REGION_ACK_H
 #define PACKETS_TS_SC_REGION_ACK_H
 
-#include "PacketDeclaration.h"
+#include "Packet/PacketDeclaration.h"
 
-#define TS_SC_REGION_ACK_DEF(_(simple), _(array), _(dynarray), _(count)) \
+#define TS_SC_REGION_ACK_DEF(_) \
 	_(simple)(int32_t, rx) \
-	_(simple)(int32_t, rx)
+	_(simple)(int32_t, ry)
 
 CREATE_PACKET(TS_SC_REGION_ACK, 11);
 
