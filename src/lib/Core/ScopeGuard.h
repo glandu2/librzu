2b2fbf169c5a85d9e0bd482e1fe150ac8bcf76aa
glandu2 <glandu2epvp@gmail.com>
2016-11-17 00:50:59 +0100

    Add ScopeGuard to execute lambda on scope exit

---
+++ src/lib/Core/ScopeGuard.h
@@ -0,0 +1,39 @@
+#ifndef SCOPEGUARD_H
+#define SCOPEGUARD_H
+
+#include <functional>
+
+template<typename Callable>
+class ScopeGuard
+{
+public:
+	ScopeGuard(Callable && undo_func) : enabled(true), f(std::forward<Callable>(undo_func)) {}
+	ScopeGuard(ScopeGuard && other) : enabled(other.enabled), f(std::move(other.f)) {
+		other.enabled = false;
+	}
+
+	~ScopeGuard() {
+		if(enabled)
+			f();
+	}
+
+	void dismiss() {
+		enabled = false;
+	}
+
+private:
+	ScopeGuard(const ScopeGuard&);
+	ScopeGuard& operator=(const ScopeGuard&);
+
+	bool enabled;
+	Callable f;
+};
+
+template <typename F>
+ScopeGuard<F> make_scope_guard(F&& f) {
+	return ScopeGuard<F>(std::forward<F>(f));
+};
+
+#define guard_on_exit(name, x) auto name = make_scope_guard(x); (void)name;
+
+#endif
