13de9fa666c5bea238fde31132c17b2c91163527
glandu2 <glandu2epvp@gmail.com>
2017-02-03 00:21:46 +0100

    Update libuv worker thread wrapper

--- src/lib/Core/ThreadWork.cpp
+++ src/lib/Core/ThreadWork.cpp
@@ -1,15 +1,39 @@
 #include "ThreadWork.h"
 #include "EventLoop.h"
 
+std::vector<uv_work_t*> ThreadWorkBase::freeHandles;
+
+ThreadWorkBase::ThreadWorkBase() : handle(nullptr) {
+}
+
 ThreadWorkBase::~ThreadWorkBase() {
-	cancel();
-	handle.data = nullptr;
+	if(handle) {
+		cancel();
+		handle->data = nullptr;
+	}
 }
 
 int ThreadWorkBase::queue(uv_work_cb work_cb, uv_after_work_cb after_work_cb) {
-	return uv_queue_work(EventLoop::getLoop(), &handle, work_cb, after_work_cb);
+	if(handle)
+		return -EBUSY;
+
+	if(freeHandles.empty()) {
+		handle = new uv_work_t;
+	} else {
+		handle = freeHandles.back();
+		freeHandles.pop_back();
+	}
+	handle->data = this;
+	return uv_queue_work(EventLoop::getLoop(), handle, work_cb, after_work_cb);
 }
 
 int ThreadWorkBase::cancel() {
-	return uv_cancel(reinterpret_cast<uv_req_t*>(&handle));
+	return uv_cancel(reinterpret_cast<uv_req_t*>(handle));
+}
+
+void ThreadWorkBase::freeHandle()
+{
+	if(handle)
+		freeHandles.push_back(handle);
+	handle = nullptr;
 }
--- src/lib/Core/ThreadWork.h
+++ src/lib/Core/ThreadWork.h
@@ -8,13 +8,19 @@
 class RZU_EXTERN ThreadWorkBase
 {
 public:
+	ThreadWorkBase();
 	~ThreadWorkBase();
 
 	int queue(uv_work_cb work_cb, uv_after_work_cb after_work_cb);
 	int cancel();
+	bool isInProgress() { return handle != nullptr; }
 
 protected:
-	uv_work_t handle;
+	void freeHandle();
+private:
+	uv_work_t* handle;
+
+	static std::vector<uv_work_t*> freeHandles;
 };
 
 template<class CallbackClass>
@@ -24,31 +30,23 @@ public:
 	typedef void (CallbackClass::*ProcessCallback)();
 	typedef void (CallbackClass::*DoneCallback)(int status);
 public:
-	ThreadWork() : inProgress(false) {
-		handle.data = this;
-	}
-	~ThreadWork() {
-		handle.data = nullptr;
-	}
-
 	int queue(CallbackClass* instance, ProcessCallback processCb, DoneCallback doneCb) {
+		if(isInProgress)
+			return -EBUSY;
+
 		this->processCallback = processCb;
 		this->doneCallback = doneCb;
 		this->instance = instance;
-		this->inProgress = true;
 		return ThreadWorkBase::queue(&ThreadWork::onProcess, &ThreadWork::onDone);
 	}
-
-	bool isInProgress() { return inProgress; }
 
 private:
 	CallbackClass* instance;
 	ProcessCallback processCallback;
 	DoneCallback doneCallback;
-	bool inProgress;
 
 	static void onProcess(uv_work_t* handle) {
-		if(handle->data) {
+		if(handle && handle->data) {
 			ThreadWork *threadWork = static_cast<ThreadWork*>(handle->data);
 			CallbackClass* instance = threadWork->instance;
 			ProcessCallback callback = threadWork->processCallback;
@@ -58,15 +56,15 @@ private:
 	}
 
 	static void onDone(uv_work_t* handle, int status) {
-		if(handle->data) {
+		if(handle && handle->data) {
 			ThreadWork *threadWork = static_cast<ThreadWork*>(handle->data);
 			CallbackClass* instance = threadWork->instance;
 			DoneCallback callback = threadWork->doneCallback;
 
 			if(instance && callback)
 				(instance->*callback)(status);
-			threadWork->inProgress = false;
 		}
+		freeHandle();
 	}
 };
 
