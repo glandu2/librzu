754da114826c70ceb8fecf0a575aaeb0bc61d270
glandu2 <glandu2epvp@gmail.com>
2017-05-08 01:52:10 +0200

    Update new packet version at epic 9.5

--- src/packets/GameClient/TS_SC_BELT_SLOT_INFO.h
+++ src/packets/GameClient/TS_SC_BELT_SLOT_INFO.h
@@ -4,7 +4,9 @@
 #include "Packet/PacketDeclaration.h"
 
 #define TS_SC_BELT_SLOT_INFO_DEF(_) \
-	_(array) (uint32_t, handle, 6) \
+	_(def)(array) (uint32_t, handle, 8) \
+	  _(impl)(array) (uint32_t, handle, 6, version < EPIC_9_5) \
+	  _(impl)(array) (uint32_t, handle, 8, version >= EPIC_9_5) \
 
 CREATE_PACKET(TS_SC_BELT_SLOT_INFO, 216);
 
--- src/packets/GameClient/TS_SC_CHARACTER_LIST.h
+++ src/packets/GameClient/TS_SC_CHARACTER_LIST.h
@@ -12,9 +12,10 @@
 	_(simple) (uint32_t, hair_color_rgb, version >= EPIC_7_1) \
 	_(simple) (uint32_t, hide_equip_flag, version >= EPIC_7_1) \
 	_(simple) (uint32_t, texture_id, version >= EPIC_6_3) \
-	_(def)(array)(uint32_t, wear_info, 24) \
-	_(impl)(array)(uint32_t, wear_info, 14, version < EPIC_4_1) \
-	_(impl)(array)(uint32_t, wear_info, 24, version >= EPIC_4_1) \
+	_(def)(array)(uint32_t, wear_info, 28) \
+	  _(impl)(array)(uint32_t, wear_info, 14, version < EPIC_4_1) \
+	  _(impl)(array)(uint32_t, wear_info, 24, version >= EPIC_4_1 && version < EPIC_9_5) \
+	  _(impl)(array)(uint32_t, wear_info, 28, version >= EPIC_9_5) \
 	_(simple) (uint32_t, level) \
 	_(simple) (uint32_t, job) \
 	_(simple) (uint32_t, job_level) \
@@ -27,14 +28,20 @@
 	_(simple) (uint32_t, skin_color, version >= EPIC_4_1) \
 	_(string) (szCreateTime, 30) \
 	_(string) (szDeleteTime, 30) \
-	_(def)(array)(uint32_t, wear_item_enhance_info, 24) \
-	_(impl)(array)(uint32_t, wear_item_enhance_info, 14, version < EPIC_4_1) \
-	_(impl)(array)(uint32_t, wear_item_enhance_info, 24, version >= EPIC_4_1) \
-	_(def)(array)(uint32_t, wear_item_level_info, 24) \
-	_(impl)(array)(uint32_t, wear_item_level_info, 14, version < EPIC_4_1) \
-	_(impl)(array)(uint32_t, wear_item_level_info, 24, version >= EPIC_4_1) \
-	_(array)  (uint8_t, wear_item_elemental_type, 24, version >= EPIC_6_1) \
-	_(array)  (uint32_t, wear_appearance_code, 24, version >= EPIC_7_4)
+	_(def)(array)(uint32_t, wear_item_enhance_info, 28) \
+	  _(impl)(array)(uint32_t, wear_item_enhance_info, 14, version < EPIC_4_1) \
+	  _(impl)(array)(uint32_t, wear_item_enhance_info, 24, version >= EPIC_4_1 && version < EPIC_9_5) \
+	  _(impl)(array)(uint32_t, wear_item_enhance_info, 28, version >= EPIC_9_5) \
+	_(def)(array)(uint32_t, wear_item_level_info, 28) \
+	  _(impl)(array)(uint32_t, wear_item_level_info, 14, version < EPIC_4_1) \
+	  _(impl)(array)(uint32_t, wear_item_level_info, 24, version >= EPIC_4_1 && version < EPIC_9_5) \
+	  _(impl)(array)(uint32_t, wear_item_level_info, 28, version >= EPIC_9_5) \
+	_(def)(array)(uint8_t, wear_item_elemental_type, 28) \
+	  _(impl)(array)(uint8_t, wear_item_elemental_type, 24, version >= EPIC_6_1 && version < EPIC_9_5) \
+	  _(impl)(array)(uint8_t, wear_item_elemental_type, 28, version >= EPIC_9_5) \
+	_(def)(array)(uint32_t, wear_appearance_code, 28) \
+	  _(impl)(array)(uint32_t, wear_appearance_code, 24, version >= EPIC_7_4 && version < EPIC_9_5) \
+	  _(impl)(array)(uint32_t, wear_appearance_code, 28, version >= EPIC_9_5) \
 
 CREATE_STRUCT(LOBBY_CHARACTER_INFO); // struct is 304 bytes long in epic2
 
