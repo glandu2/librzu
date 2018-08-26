8568a75c8e0fc8ad3aa7f1531a554505419a64b6
glandu2 <glandu2epvp@gmail.com>
2018-08-26 15:35:42 +0200

    Utils: fix convertDataToHex

--- src/lib/Core/Utils.cpp
+++ src/lib/Core/Utils.cpp
@@ -201,7 +201,7 @@ void Utils::convertDataToHex(const void* data, int size, char* outHex) {
 	static const char hexMapping[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 	int i;
 	for(i = 0; i < size; i++) {
-		outHex[i * 2] = hexMapping[static_cast<const char*>(data)[i] >> 4];
+		outHex[i * 2] = hexMapping[(static_cast<const char*>(data)[i] >> 4) & 0x0F];
 		outHex[i * 2 + 1] = hexMapping[static_cast<const char*>(data)[i] & 0x0F];
 	}
 	outHex[i * 2] = '\0';
