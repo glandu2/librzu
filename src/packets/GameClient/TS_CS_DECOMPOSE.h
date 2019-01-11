6a7fd87fe556146a7a3e4b347e880124fc770dee
glandu2 <glandu2epvp@gmail.com>
2019-01-11 22:52:17 +0100

    TS_SC_DECOMPOSE: fix for epic >= 8.3

--- src/packets/GameClient/TS_CS_DECOMPOSE.h
+++ src/packets/GameClient/TS_CS_DECOMPOSE.h
@@ -3,9 +3,14 @@
 
 #include "Packet/PacketDeclaration.h"
 
+#define TS_DECOMPOSE_ITEM_INFO_DEF(_) \
+	_(simple)(uint32_t, items_handle) \
+	_(simple)(uint32_t, count, version >= EPIC_8_3, 1)
+CREATE_STRUCT(TS_DECOMPOSE_ITEM_INFO);
+
 #define TS_CS_DECOMPOSE_DEF(_) \
-	_(count)(uint32_t, items_handle) \
-	_(dynarray)(uint32_t, items_handle)
+	_(count)(uint32_t, items) \
+	_(dynarray)(TS_DECOMPOSE_ITEM_INFO, items)
 
 // Since EPIC_8_1
 CREATE_PACKET(TS_CS_DECOMPOSE, 265);
