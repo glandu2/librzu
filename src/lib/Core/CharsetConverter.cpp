a76f7b3084f4d39346a7eb1afd1af6054604446f
glandu2 <glandu2epvp@gmail.com>
2019-01-11 22:49:46 +0100

    CharsetConverter: cache instances

--- src/lib/Core/CharsetConverter.cpp
+++ src/lib/Core/CharsetConverter.cpp
@@ -3,24 +3,99 @@
 #include "Log.h"
 #include "iconv.h"
 #include <errno.h>
+#include <memory>
 #include <stdio.h>
 
-CharsetConverter::CharsetConverter(const char* from, const char* to) {
-	ic = iconv_open(to, from);
-	if(!ic || ic == (iconv_t) -1) {
-		log(LL_Error, "Cant open iconv: from: \"%s\", to: \"%s\"\n", from, to);
-		ic = nullptr;
+IconvInstance::IconvInstance(const char* from, const char* to, void* ic) : from(from), to(to), ic(ic, &iconv_close) {
+	uv_mutex_init(&mutex);
+}
+IconvInstance::~IconvInstance() {
+	uv_mutex_destroy(&mutex);
+}
+
+int IconvInstance::convertIgnoreInvalid(const char** inBuf, int* inSize, char** outBuf, int* outSize) {
+	size_t sInSize = *inSize;
+	size_t sOutSize = *outSize;
+	const char* inPtr = *inBuf;
+	char* outPtr = *outBuf;
+	size_t result;
+
+	uv_mutex_lock(&mutex);
+
+	iconv(ic.get(), nullptr, nullptr, nullptr, nullptr);
+	while((result = iconv(ic.get(), &inPtr, &sInSize, &outPtr, &sOutSize)) == (size_t) -1 && sInSize > 1 &&
+	      sOutSize > 0) {
+		if(errno == EINVAL || errno == E2BIG)
+			break;
+
+		inPtr++;
+		sInSize--;
+	}
+	uv_mutex_unlock(&mutex);
+
+	*inBuf = inPtr;
+	*inSize = (int) sInSize;
+	*outBuf = outPtr;
+	*outSize = (int) sOutSize;
+
+	if(sInSize == 0)
+		return 0;
+	else {
+		if(errno == EINVAL || errno == E2BIG)
+			return -errno;
+		else
+			return -EINVAL;
 	}
 }
 
-CharsetConverter::~CharsetConverter() {
-	if(ic)
-		iconv_close(ic);
+CharsetConverterGlobalCache::CharsetConverterGlobalCache() {
+	uv_mutex_init(&mutex);
 }
+
+CharsetConverterGlobalCache::~CharsetConverterGlobalCache() {
+	uv_mutex_destroy(&mutex);
+}
+
+IconvInstance* CharsetConverterGlobalCache::get(const char* from, const char* to) {
+	uv_mutex_lock(&mutex);
+	for(IconvInstance& ic : iconvInstances) {
+		if(ic.from == from && ic.to == to) {
+			uv_mutex_unlock(&mutex);
+			return &ic;
+		}
+	}
+
+	iconv_t ic = iconv_open(to, from);
+	if(!ic || ic == (iconv_t) -1) {
+		log(LL_Error, "Cant open iconv: from: \"%s\", to: \"%s\"\n", from, to);
+
+		uv_mutex_unlock(&mutex);
+		return nullptr;
+	} else {
+		iconvInstances.emplace_back(from, to, ic);
+		IconvInstance* instance = &iconvInstances.back();
+
+		uv_mutex_unlock(&mutex);
+		return instance;
+	}
+}
+
+CharsetConverterGlobalCache CharsetConverter::cache;
+
+CharsetConverter::CharsetConverter(const char* from, const char* to) {
+	ic = cache.get(from, to);
+}
+
+CharsetConverter::~CharsetConverter() {}
 
 void CharsetConverter::convert(const std::string& in, std::string& out, float ratioHint) {
 	if(in.empty()) {
 		out.clear();
+		return;
+	}
+
+	if(!ic) {
+		out = in;
 		return;
 	}
 
@@ -32,7 +107,7 @@ void CharsetConverter::convert(const std::string& in, std::string& out, float ra
 	int outRemainingBytes = (int) out.size();
 
 	while(inRemainingBytes > 0) {
-		int result = iconvIgnoreInvalid(&inPtr, &inRemainingBytes, &outPtr, &outRemainingBytes);
+		int result = ic->convertIgnoreInvalid(&inPtr, &inRemainingBytes, &outPtr, &outRemainingBytes);
 		if(result < 0) {
 			if(result == -E2BIG) {
 				int outDone = (int) out.size() - outRemainingBytes;
@@ -50,35 +125,4 @@ void CharsetConverter::convert(const std::string& in, std::string& out, float ra
 
 std::string CharsetConverter::getEncoding() {
 	return GlobalCoreConfig::get()->app.encoding.get();
-}
-
-int CharsetConverter::iconvIgnoreInvalid(const char** inBuf, int* inSize, char** outBuf, int* outSize) {
-	size_t sInSize = *inSize;
-	size_t sOutSize = *outSize;
-	const char* inPtr = *inBuf;
-	char* outPtr = *outBuf;
-	size_t result;
-
-	iconv(ic, nullptr, nullptr, nullptr, nullptr);
-	while((result = iconv(ic, &inPtr, &sInSize, &outPtr, &sOutSize)) == (size_t) -1 && sInSize > 1 && sOutSize > 0) {
-		if(errno == EINVAL || errno == E2BIG)
-			break;
-
-		inPtr++;
-		sInSize--;
-	}
-
-	*inBuf = inPtr;
-	*inSize = (int) sInSize;
-	*outBuf = outPtr;
-	*outSize = (int) sOutSize;
-
-	if(sInSize == 0)
-		return 0;
-	else {
-		if(errno == EINVAL || errno == E2BIG)
-			return -errno;
-		else
-			return -EINVAL;
-	}
 }
--- src/lib/Core/CharsetConverter.h
+++ src/lib/Core/CharsetConverter.h
@@ -2,9 +2,40 @@
 #define CHARSETCONVERTER_H
 
 #include "Object.h"
+#include "uv.h"
+#include <deque>
+#include <memory>
 #include <string>
 
-class RZU_EXTERN CharsetConverter : public Object {
+struct RZU_EXTERN IconvInstance {
+	IconvInstance(const char* from, const char* to, void* ic);
+	~IconvInstance();
+
+	int convertIgnoreInvalid(const char** inbuf, int* inSize, char** outbuf, int* outSize);
+
+	std::string from;
+	std::string to;
+	std::unique_ptr<void, int (*)(void*)> ic;
+	mutable uv_mutex_t mutex;
+};
+
+class RZU_EXTERN CharsetConverterGlobalCache : public Object {
+public:
+	CharsetConverterGlobalCache();
+	~CharsetConverterGlobalCache();
+
+	IconvInstance* get(const char* from, const char* to);
+
+	CharsetConverterGlobalCache(const CharsetConverterGlobalCache&) = delete;
+	CharsetConverterGlobalCache& operator=(const CharsetConverterGlobalCache&) = delete;
+
+private:
+	std::deque<IconvInstance> iconvInstances;
+	uv_mutex_t mutex;
+};
+
+class RZU_EXTERN CharsetConverter {
+private:
 public:
 	CharsetConverter(const char* from, const char* to);
 	~CharsetConverter();
@@ -14,11 +45,9 @@ public:
 
 	static std::string getEncoding();
 
-protected:
-	int iconvIgnoreInvalid(const char** inbuf, int* inSize, char** outbuf, int* outSize);
-
 private:
-	void* ic;
+	static CharsetConverterGlobalCache cache;
+	IconvInstance* ic;
 };
 
 #endif  // CHARSETCONVERTER_H
