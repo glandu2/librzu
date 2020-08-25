c77babaaffeedeaa856bc7cb4e7dce91c953548b
glandu2 <glandu2epvp@gmail.com>
2020-08-25 16:41:43 +0200

    BackrgoundWork: allow multiple parallel works

--- src/lib/Core/BackgroundWork.h
+++ src/lib/Core/BackgroundWork.h
@@ -5,6 +5,8 @@
 #include "Core/Utils.h"
 #include "uv.h"
 #include <array>
+#include <list>
+#include <memory>
 #include <tuple>
 
 template<class CallbackClass, class Argument> class BackgroundWork {
@@ -17,43 +19,67 @@ public:
 		this->instance = targetInstance;
 		this->workCallback = wcb;
 		this->afterWorkCallback = awcb;
-
-		work.data = this;
-		uv_mutex_init(&stopMutex);
 	}
-	~BackgroundWork() {
-		uv_cancel((uv_req_t*) &work);
-		uv_mutex_lock(&stopMutex);
-		work.data = nullptr;
-		uv_mutex_unlock(&stopMutex);
-		uv_mutex_destroy(&stopMutex);
+	~BackgroundWork() { cancel(); }
+
+	void cancel() {
+		for(auto& job : ongoingJobs) {
+			uv_cancel((uv_req_t*) &job->work);
+			job->instance = nullptr;
+		}
+		ongoingJobs.clear();
 	}
 
 	int run(Argument arg) {
-		if(uv_mutex_trylock(&stopMutex) < 0)
-			return UV_EBUSY;
+		std::unique_ptr<JobInstance> job;
 
-		this->savedArg = arg;
-		this->result = 0;
+		if(freeJobs.empty()) {
+			job.reset(new JobInstance);
+		} else {
+			job = std::move(freeJobs.back());
+			freeJobs.pop_back();
+		}
 
-		uv_queue_work(EventLoop::getLoop(), &work, &onWork, &onAfterWork);
+		job->instance = this;
+		job->savedArg = arg;
+		job->result = EINTR;
+		job->work.data = job.get();
+		auto* work = &job->work;
+
+		ongoingJobs.push_back(job.release());
+
+		uv_queue_work(EventLoop::getLoop(), work, &onWork, &onAfterWork);
 
 		return 0;
 	}
 
 private:
 	static void onWork(uv_work_t* req) {
-		BackgroundWork* thisInstance = static_cast<BackgroundWork*>(req->data);
-		// uint64_t startTime = Utils::getTimeInMsec();
-		thisInstance->result = (thisInstance->instance->*thisInstance->workCallback)(thisInstance->savedArg);
-		// thisInstance->instance->log(Object::LL_Info, "Work done in %d\n", Utils::getTimeInMsec() - startTime);
+		JobInstance* jobInstance = static_cast<JobInstance*>(req->data);
+		BackgroundWork* thisInstance = jobInstance->instance;
+
+		if(thisInstance) {
+			jobInstance->result = (thisInstance->instance->*thisInstance->workCallback)(jobInstance->savedArg);
+		}
 	}
 
 	static void onAfterWork(uv_work_t* req, int status) {
-		BackgroundWork* thisInstance = static_cast<BackgroundWork*>(req->data);
-		(thisInstance->instance->*thisInstance->afterWorkCallback)(
-		    std::move(thisInstance->savedArg), thisInstance->result ? thisInstance->result : status);
-		uv_mutex_unlock(&thisInstance->stopMutex);
+		JobInstance* jobInstance = static_cast<JobInstance*>(req->data);
+		BackgroundWork* thisInstance = jobInstance->instance;
+
+		if(thisInstance && status == 0) {
+			Argument savedArg = std::move(jobInstance->savedArg);
+			int result = jobInstance->result;
+			jobInstance->instance = nullptr;
+			jobInstance->work.data = nullptr;
+
+			thisInstance->ongoingJobs.remove(jobInstance);
+			thisInstance->freeJobs.emplace_back(jobInstance);
+
+			(thisInstance->instance->*thisInstance->afterWorkCallback)(std::move(savedArg), result);
+		} else {
+			delete jobInstance;
+		}
 	}
 
 private:
@@ -61,10 +87,13 @@ private:
 	WorkCallback workCallback;
 	AfterWorkCallback afterWorkCallback;
 
-	Argument savedArg;
-	int result;
+	struct JobInstance {
+		BackgroundWork* instance;
+		Argument savedArg;
+		int result;
 
-	uv_mutex_t stopMutex;
-	uv_work_t work;
+		uv_work_t work;
+	};
+	std::vector<std::unique_ptr<JobInstance>> freeJobs;
+	std::list<JobInstance*> ongoingJobs;
 };
-
