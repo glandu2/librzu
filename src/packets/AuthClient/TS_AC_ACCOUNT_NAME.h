46b83662a7ec7b3815b7bc38d5c4517bf00cccdc
glandu2 <glandu2epvp@gmail.com>
2020-08-26 23:25:44 +0200

    TS_AC_ACCOUNT_NAME: add new data fields
    Since 9.6.3 ? Not sure about this.
    This packet may contain distribution specific data and is not required.

--- src/packets/AuthClient/TS_AC_ACCOUNT_NAME.h
+++ src/packets/AuthClient/TS_AC_ACCOUNT_NAME.h
@@ -4,6 +4,20 @@
 
 #define TS_AC_ACCOUNT_NAME_DEF(_) \
 	_(string)(account, 61) \
-	_(simple)(uint32_t, account_id)
+	_(simple)(uint32_t, account_id) \
+	_(simple)(uint32_t, unknown1, version >= EPIC_9_6_3) \
+	_(string)(unknown2_hex, 57, version >= EPIC_9_6_3) \
+	_(simple)(uint16_t, unknown3, version >= EPIC_9_6_3) \
+	_(simple)(uint8_t, unknown4, version >= EPIC_9_6_3) \
+	_(simple)(uint32_t, unknown5, version >= EPIC_9_6_3) \
+	_(simple)(uint32_t, unknown6, version >= EPIC_9_6_3) \
+	_(simple)(uint32_t, unknown7, version >= EPIC_9_6_3) \
+	_(simple)(uint32_t, unknown8, version >= EPIC_9_6_3) \
+	_(simple)(uint32_t, unknown9, version >= EPIC_9_6_3) \
+	_(simple)(uint32_t, unknown10, version >= EPIC_9_6_3) \
+	_(simple)(uint32_t, unknown11, version >= EPIC_9_6_3) \
+	_(simple)(uint32_t, unknown12, version >= EPIC_9_6_3) \
+	_(simple)(uint32_t, unknown13, version >= EPIC_9_6_3)
+
 CREATE_PACKET(TS_AC_ACCOUNT_NAME, 10014, SessionType::AuthClient, SessionPacketOrigin::Server);
 
